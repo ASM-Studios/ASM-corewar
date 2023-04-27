8d5b7268d15f8c8ce01a31650ce9576c8876a0f6
mathieu <mathieu.borel@epitech.eu>
2023-04-27 10:21:15 +0200

    Add: file test

---
+++ asm/tests/asm.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** ASM-corewar
+** File description:
+** asm.c
+*/
+
+#include "../include/prototype.h"
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+void redirect_all_std(void);
+
+Test(asm, basic) {
+    char path[] = "./tests/test_file.s";
+}
