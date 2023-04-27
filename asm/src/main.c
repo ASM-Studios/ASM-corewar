435d4659020ea190ac8688e9f4a2d4562c3713a9
Yohann <102670831+YohannMgt@users.noreply.github.com>
2023-04-27 12:50:22 +0200

    Add: help for corewar & asm
    Signed-off-by: Yohann <102670831+YohannMgt@users.noreply.github.com>

--- asm/src/main.c
+++ asm/src/main.c
@@ -7,9 +7,26 @@
 
 #include "../include/prototype.h"
 
+static int display_help(void)
+{
+    FILE *stream = fopen("asm/help.txt", "r");
+    size_t len;
+    char *line = NULL;
+
+    if (!stream)
+        return 1;
+    while (getline(&line, &len, stream) != -1)
+        my_printf(line);
+    fclose(stream);
+    free(line);
+    return 0;
+}
+
 int main(const int ac, const char **av)
 {
     if (ac != 2)
         return 84;
+    if (!my_strcmp(av[1], "-h"))
+        return display_help();
     return asm_main(av[1]);
 }
