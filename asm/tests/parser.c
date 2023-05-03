50895c25fe501e4835bb800f4c5970160b5fdc7b
mathieu <mathieu.borel@epitech.eu>
2023-05-03 16:15:42 +0200

    Add: test

--- asm/tests/parser.c
+++ asm/tests/parser.c
@@ -12,5 +12,8 @@
 void redirect_all_std(void);
 
 Test(parser, basic) {
-
+    app_t *app = create_app();
+    open_file(app, "./tests/tests_file/basic.s");
+    int status = parser(app);
+    cr_assert_eq(status, 0);
 }
