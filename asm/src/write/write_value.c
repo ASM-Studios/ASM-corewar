5479ebe1c7f4a64b056e052637752501410e1d3f
Mathieu BOREL <mathieu.borel@epitech.eu>
2023-05-09 09:05:33 +0200

    add conversion

--- asm/src/write/write_value.c
+++ asm/src/write/write_value.c
@@ -12,19 +12,19 @@ int write_value(char *arg, type_t type, FILE *stream)
     int value = 0;
 
     if (type == Direct) {
-        value = my_atoi(&arg[1]);
+        value = little_endian_converter(my_atoi(&arg[1]));
         fwrite(&value, DIRECT_SIZE, 1, stream);
     }
     if (type == Register) {
-        value = my_atoi(&(arg[1]));
+        value = little_endian_converter(my_atoi(&(arg[1])));
         fwrite(&value, REGISTER_SIZE, 1, stream);
     }
     if (type == Indirect) {
-        value = my_atoi(arg);
+        value = little_endian_converter(my_atoi(arg));
         fwrite(&value, INDIRECT_SIZE, 1, stream);
     }
     if (type == Index) {
-        value = my_atoi(arg);
+        value = little_endian_converter(my_atoi(arg));
         fwrite(&value, INDEX_SIZE, 1, stream);
     }
     return 0;
