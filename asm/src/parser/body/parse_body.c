6c54fa37f58d7be88751ac3a0dc6990d1a6e503e
Mathieu BOREL <mathieu.borel@epitech.eu>
2023-05-25 11:10:24 +0200

    Fix: segfault

--- asm/src/parser/body/parse_body.c
+++ asm/src/parser/body/parse_body.c
@@ -56,7 +56,7 @@ STATIC int parse_body_line(app_t *app, char **body, int i, char **label)
     char *exp = (is_label(array[0]) == 1) ? array[1] : array[0];
     char **arg = (is_label(array[0]) == 1) ? &array[2] : &array[1];
 
-    if (exp[0] == '#') {
+    if (exp != NULL && exp[0] == '#') {
         free_double_array(array);
         return 0;
     }
