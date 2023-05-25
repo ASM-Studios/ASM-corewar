6b551b4ef167a3b27dcc4080682ebee685009597
Mathieu BOREL <mathieu.borel@epitech.eu>
2023-05-25 13:38:38 +0200

    Delete: forbidden functions && edit zjmp

--- corewar/src/instruction/instructions/zjmp.c
+++ corewar/src/instruction/instructions/zjmp.c
@@ -9,8 +9,7 @@
 
 int instruction_zjmp(app_t *app, champion_t *champion, parameter_t **parameters)
 {
-    printf("%d\n", champion->PC);
-    champion->PC = champion->PC + parameters[0]->value % IDX_MOD;
-    printf("%d\n", champion->PC);
+    if (champion->carry == 1)
+        champion->PC = champion->PC + parameters[0]->value % IDX_MOD;
     return 0;
 }
